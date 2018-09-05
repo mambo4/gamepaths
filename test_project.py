6b83905cda0f00681863c2d64c9f84f23d3fc000
mambo4 <logan.e.bender@gmail.com>
2018-09-05 08:10:16 -0500

    edit out git merge garbage

--- test_project.py
+++ test_project.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import project as project
 from importlib import reload
 reload(project)
@@ -10,22 +9,10 @@ JSON="project_client.json"
 
 def test_IdProjectData():
     instance=project.IdProjectData()
-=======
-import id_project as id_project
-from importlib import reload
-
-reload(id_project)
-
-JSON="id_project_client.json"
-
-def test_IdProjectData():
-    instance=id_project.IdProjectData()
->>>>>>> fd773fb7323cda17b727de293665b86209cb4b5c
     instance.print_data()
     instance.load(file=JSON)
     instance.print_data()
 
-<<<<<<< HEAD
 def test_IdProjectClient():
     client=project.IdProjectClient()
     # client.print_data()
@@ -33,8 +20,4 @@ def test_IdProjectClient():
 if __name__=="__main__":
     # test_IdProjectData()
     test_IdProjectClient()
-=======
-if __name__=="__main__":
-    test_IdProjectData()
->>>>>>> fd773fb7323cda17b727de293665b86209cb4b5c
 
